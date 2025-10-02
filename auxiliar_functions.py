6729d782e36e25847fb9207020a397e6a50f7ced
pujolaleix <aleixpujolcv@gmail.com>
2025-10-02 17:49:50 +0200

    final commit

--- auxiliar_functions.py
+++ auxiliar_functions.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd
 import math
 import matplotlib.patheffects as pe
+from matplotlib.patches import FancyArrowPatch
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mplsoccer import Pitch
+import os
 
 # -------------------------------------
 # ----- Data Processing functions -----
@@ -66,7 +71,7 @@ def load_360(id: int, data_dir: Path):
 
 
 def third_x(x):
-    # StatsBomb pitch: x in [0,120]
+    # StatsBomb pitch: [0,120]
     if x < 40:  return "defensive"
     if x < 80:  return "middle"
     return "attacking"
@@ -92,8 +97,8 @@ def pass_features_from_events(events_json, features) -> pd.DataFrame:
     df["dy"] = df["ey"] - df["sy"]
     df["length"] = np.hypot(df["dx"], df["dy"])
     df["angle"]  = np.arctan2(df["dy"], df["dx"])  # radians
-    df["direction"] = np.where(df["dx"] > 1, "forward", np.where(df["dx"] < -1, "backward", "lateral"))
-    df["is_progressive"] = df["dx"] > 10  # simple proxy
+    df["direction"] = np.where(df["dx"] > 10, "forward", np.where(df["dx"] < -10, "backward", "lateral"))
+    df["is_progressive"] = df["dx"] > 20
     df["height"] = df["pass.height.name"].fillna("Unknown")
     df["is_cross"] = df["pass.cross"].fillna(False)
     df["is_through"] = df["pass.through_ball"].fillna(False)
@@ -152,6 +157,79 @@ def pressure_metrics(df: pd.DataFrame, pressure_thr: float, bypass_margin_thr: f
 # --------------------------------------
 # ---- Data Visualization functions ----
 # --------------------------------------
+
+def per_field_zone_visual(vcol, nx, ny, team_color, df, save_dir):
+
+    xcol = 'sx'
+    ycol = 'sy'
+
+    pitch = Pitch(pitch_type="statsbomb", pitch_color="#2C2C2C", line_color='white', linewidth=1.5)
+    fig, ax = pitch.draw(figsize=(7,5))
+
+    # compute per-team metric grid
+    team_grids = {}
+    for team, g in df.groupby("team"):
+        s = pitch.bin_statistic(
+            g[xcol].to_numpy(), g[ycol].to_numpy(),
+            values=g[vcol].to_numpy(),
+            statistic="mean", bins=(nx, ny)
+        )
+        team_grids[team] = pd.DataFrame(s["statistic"], index=range(ny), columns=range(nx))
+
+    # Compute bin center to display text
+    pitch_length, pitch_width = pitch.dim.length, pitch.dim.width # [120, 80] in StatsBomb pitch 
+    x_edges = np.linspace(0, pitch_length, nx + 1)
+    y_edges = np.linspace(0, pitch_width, ny + 1)
+    x_centers = 0.5 * (x_edges[:-1] + x_edges[1:])
+    y_centers = 0.5 * (y_edges[:-1] + y_edges[1:])
+    # small vertical offsets so multiple team numbers don't overlap in a cell
+    nT = max(1, len(df['team'].unique()))
+    cell_h = y_edges[1] - y_edges[0]
+    offsets = {t: ((i - (nT - 1) / 2) * (cell_h * 0.20)) for i, t in enumerate(df['team'].unique())}
+
+
+    # display soft white grid lines to split field in areas
+    for xe in x_edges[1:-1]:
+        ax.plot([xe, xe], [0, pitch_width], color="white", alpha=0.25, lw=1.2, zorder=5)
+    for ye in y_edges[1:-1]:
+        ax.plot([0, pitch_length], [ye, ye], color="white", alpha=0.25, lw=1.2, zorder=5)
+    
+
+    # write each team's mean in its color at each bin center
+    for t in df['team'].unique():
+        grid = team_grids[t]
+        dy = offsets[t]
+        for iy in range(ny):
+            for ix in range(nx):
+                val = grid.iloc[iy, ix]
+                if np.isfinite(val):
+                    ax.text(
+                        x_centers[ix], y_centers[iy] + dy, f"{val:.2f}",
+                        ha="center", va="center",
+                        color=team_color[t], fontsize=10, fontweight="bold", zorder=6,
+                        path_effects=[pe.withStroke(linewidth=2, foreground="black", alpha=0.6)]
+                    )
+
+    # title
+    ax.set_title(f"{vcol} average by field zone",
+                fontsize=10, color="black", pad=15, fontweight="bold")
+    
+    # arrow that shows attacking direction
+    arrow = FancyArrowPatch(
+        (15, 75), (105, 75),
+        arrowstyle='-|>', mutation_scale=25,
+        color="white", lw=2, alpha=0.55
+    )
+    ax.add_patch(arrow)
+    ax.text(60, 80, "Attacking direction", ha="center", va="bottom", color="white", alpha=0.55, fontsize=8, fontweight="bold")
+
+    # save as .png
+    os.makedirs(save_dir, exist_ok=True)
+    fname = f"per_zone_{vcol}_{nx}x{ny}.png"
+    save_path = os.path.join(save_dir, fname)
+    fig.savefig(save_path, dpi=200, bbox_inches="tight", facecolor=fig.get_facecolor())
+
+
 
 def draw_numbered_circle(ax, x, y, number, facecolor, size=220, text_color="white", lw=1.2):
     sc = ax.scatter(x, y, s=size, marker="o", facecolor=facecolor,
